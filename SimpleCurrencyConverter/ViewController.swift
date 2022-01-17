8cfeb1d61ee38c7a701e35c6e2987635ec31a5a3
blazeeYesForSure <blazej.wietczak@icloud.com>
2022-01-17 20:37:51 +0100

    Initial Commit

---
+++ SimpleCurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCurrencyConverter
+//
+//  Created by Blazej Wietczak on 17/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

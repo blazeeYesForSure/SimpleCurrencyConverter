cd7dfb07d113142378caa94e76ab64094b471056
blazeeYesForSure <blazej.wietczak@icloud.com>
2022-01-18 22:18:13 +0100

    connecting with API and JSON parsing

--- SimpleCurrencyConverter/ViewController.swift
+++ SimpleCurrencyConverter/ViewController.swift
@@ -17,10 +17,37 @@ class ViewController: UIViewController{
     // MARK: - IBActions
     @IBAction func Convertbutton(_ sender: Any) {
     }
-
+    // MARK: - Properties
+    var rateUsdCurrency = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetechJSON()
     }
-    
+
+    //MARK: - Method
+    func fetechJSON() {
+        guard let url = URL(string: "https://open.er-api.com/v6/latest/EUR") else {return}
+        URLSession.shared.dataTask(with: url) { [self](data,response, error) in
+            //handle any errors if there are any
+            if error != nil  {
+                print(error!)
+                return
+            }
+            //safely unwrap the data
+            guard let safeData = data else {return}
+            
+            //decode the JSON data
+            do{
+                let results = try JSONDecoder().decode(ExchangeRates.self, from: safeData)
+                rateUsdCurrency = results.rates["USD"]!
+                print(rateUsdCurrency)
+                }
+            catch{
+                print(error)
+                
+            }
+        }.resume()
+    }    
 }
